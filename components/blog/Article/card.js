94d414de2cb08936cef28b6508d3acdbd82c7cda
Jouskaio <jouskaio.me@gmail.com>
2022-06-14 16:16:49 +0200

    Adding image preload and correcting image displaying

--- components/blog/Article/card.js
+++ components/blog/Article/card.js
@@ -2,17 +2,21 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import {getStrapiMedia} from "../../../lib/blog/api";
+import {shimmer, toBase64} from "../Preload/preload-image";
 
 const Card = ({ article }) => {
-  const imageUrl =
-    process.env.NODE_ENV !== "development"
-      ? article.attributes.image.data.attributes.url
-      : process.env.REACT_APP_BACKEND_URL +
-      article.attributes.image.data.attributes.url;
+
   return (
     <Link href={`/article/${article.attributes.slug}`} className="uk-link-reset">
       <a>
-        <Image src={getStrapiMedia(article.attributes.image)} alt={article.attributes.image.url} height={100} width="10O%" />
+        <div style={{position: 'relative', width: '300px', height: '300px' }}>
+          {/**
+           * Source for blurData and placeholder strategy : https://github.com/vercel/next.js/blob/canary/examples/image-component/pages/shimmer.js
+           * Source for filling image strategy : https://github.com/vercel/next.js/blob/canary/examples/image-component/pages/layout-fill.js
+           */}
+          <Image loader={() => getStrapiMedia(article.attributes.image)} src={getStrapiMedia(article.attributes.image)} alt={article.attributes.image.url} height={100} layout="fill" objectFit="none" placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(300, 300))}`} />
+        </div>
         <p id="category" className="uk-text-uppercase">
           {article.attributes.category.data.attributes.name}
           <br/>
