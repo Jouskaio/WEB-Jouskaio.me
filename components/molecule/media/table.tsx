e292d751a62894561de6c9c7d999ee7a5edc9395
Jouskaio <jouskaio.me@gmail.com>
2023-03-14 20:23:38 +0100

    Adding table projects

--- components/molecule/media/table.tsx
+++ components/molecule/media/table.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 
+/**
+ *
+ * @param items : [{year: number, name : string, link: string}]
+ * @constructor
+ */
 function Table({items}) {
     return (
         <table className="m-table">
             <thead>
             <tr>
-                <th scope="col" className={"m-table__title"}>Years</th>
-                <th scope="col" className={"m-table__title"}>Name</th>
+                <th scope="col" className={"m-table__title"}>YEARS</th>
+                <th scope="col" className={"m-table__title"}>NAME</th>
             </tr>
             </thead>
             <tbody>
-            <tr className={"m-table__cells--row"}>
-                <td className={"m-table__cells"}>2021</td>
-                <td className={"m-table__cells"}>James Yates</td>
-            </tr>
-            <tr className={"m-table__cells--row"}>
-                <td className={"m-table__cells"}>2022</td>
-                <td className={"m-table__cells"}>Matthew Wasil</td>
-            </tr>
+            {
+                items.map(function (cell, i) {
+                    return (
+                        <tr className={"m-table__cells--row"} key={i}>
+                            <td className={"m-table__cells"}><a href={cell.link} >{cell.year}</a></td>
+                            <td className={"m-table__cells"}><a href={cell.link}>{cell.name}</a></td>
+                        </tr>
+                    )
+                })
+            }
             </tbody>
         </table>
     )
