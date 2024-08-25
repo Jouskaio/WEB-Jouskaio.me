65c6464cdceb3a2d8c3529edbaae729fb6a45f8a
Jouskaio <jouskaio.me@gmail.com>
2024-08-25 17:31:35 +0200

    :bug: :fix Fixing CI-CD

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-// Assurez-vous d'importer `next-compose-plugins` ici
 const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
