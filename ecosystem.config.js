0e8fe4bd90b808c9dec0ddaa8cda0fb98aef3bf0
Jouskaio <jouskaio.me@gmail.com>
2026-07-17 20:29:56 +0200

    :green_heart: Configure PM2 and self hosted CI-CD deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,40 @@
 module.exports = {
-  apps: [
-    {
-      name: "WEB-Jouskaio.me",
-      script: "yarn",
-      args: "start",
-      watch: true,
-      "ignore_watch" : ["node_modules"],
-      "watch_options": {
-        "usePolling": true,
-        "interval": 1000
-      },
-      instances: "max",
-      exec_mode: "cluster",
-      env: {
-        NODE_ENV: "development", // Variables d'environnement pour développement
-      },
-      env_production: {
-        NODE_ENV: "production", // Variables d'environnement pour production
-      },
-    },
-  ],
+    apps: [
+        {
+            name: 'WEB-jouskaio.me',
+            cwd: '/home/user/Documents/jouskaio.me/WEB-Jouskaio.me',
+            script: '/usr/bin/bash',
+            args: ['-lc', 'npx next start -H 0.0.0.0 -p 3000'],
+            interpreter: 'none',
+            exec_mode: 'fork',
+            instances: 1,
+            autorestart: true,
+            restart_delay: 5000,
+            max_restarts: 20,
+            max_memory_restart: '500M',
+            watch: false,
+            env: {
+                NODE_ENV: 'production',
+            },
+            time: true,
+        },
+        {
+            name: 'WEB-styleguide',
+            cwd: '/home/user/Documents/jouskaio.me/WEB-Jouskaio.me',
+            script: '/usr/bin/bash',
+            args: ['-lc', 'npx serve -s styleguide -l tcp://0.0.0.0:6060'],
+            interpreter: 'none',
+            exec_mode: 'fork',
+            instances: 1,
+            autorestart: true,
+            restart_delay: 5000,
+            max_restarts: 20,
+            max_memory_restart: '300M',
+            watch: false,
+            env: {
+                NODE_ENV: 'production',
+            },
+            time: true,
+        },
+    ],
 };
